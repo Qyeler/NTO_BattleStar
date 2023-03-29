9ca7305201530b1646d526fc3f9989a42a5c014c
Qyeler <107304449+Qyeler@users.noreply.github.com>
2023-03-29 11:32:06 +0700

    v3.5 user front back IIII NEEED FROOOONT admin 1.0 back

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import base64
-
+from io import BytesIO
 from flask import Flask
 import os
 import io
@@ -138,35 +138,63 @@ def admin_dashboard(admin_username):
     users = load_users()
     all_users = []
     for username, data in users.items():
+        if(users[username]['access_level']=="admin" or users[username]['access_level']=="oper"):
+            continue
+        filename = f"user_data/{username}.txt"
+        with open(filename, 'r') as file:
+            data = file.readlines()
+        x = []
+        y = []
+
+        for line in data:
+            values = line.strip().split(',')
+            time = int(values[0]) * 24 * 60 * 60 + int(values[1]) * 60 * 60 + int(values[2]) * 60 + int(values[3])
+            power = float(values[4])
+            x.append(time)
+            y.append(power)
+
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Power')
+        ax.set_title('Power Usage')
+        plt.xticks(rotation=45)
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        img.seek(0)
+
+        # Передаем путь к изображению в шаблон
+        img_url = base64.b64encode(img.read()).decode()
+
         user_info = {
             'username': username,
-            'field1': "asfsaafsasaf",  # заполни данные для поля 1
-            'field2': "asfsaafsasasdadaf",  # заполни данные для поля 2
+            'field1': "template(to be continue) money/last month",  # заполни данные для поля 1
+            'graf': f'data:image/png;base64,{img_url}',  # заполни данные для поля 2
+            'field2':"on/off"
         }
         all_users.append(user_info)
     print(all_users)
     return render_template('admin_dashboard.html', username=admin_username, all_users=all_users)
 
 
-@app.route('/admin_dashboard/<admin_username>/user_stats/<user_username>')
-def user_stats(admin_username, user_username):
-    if 'username' not in session or session['username'] != admin_username:
-        return redirect(url_for('login'))
-    users = load_users()
-    if admin_username not in users or users[admin_username]['access_level'] != 'admin':
-        abort(404)
-    if user_username not in users:
-        abort(404)
-
-    # здесь собираем статистику для пользователя user_username
-    # и передаем ее в шаблон для отображения
-    user_stats = {
-        'username': user_username,
-        'field1': "asfsaafsasaf",  # заполни данные для поля 1
-        'field2': "asfsaafsasasdadaf",  # заполни данные для поля 2
-    }
-
-    return render_template('user_stats.html', username=admin_username, user_username=user_username, user_stats=user_stats)
+# @app.route('/admin_dashboard/<admin_username>/user_stats/<user_username>')
+# def user_stats(admin_username, user_username):
+#     if 'username' not in session or session['username'] != admin_username:
+#         return redirect(url_for('login'))
+#     users = load_users()
+#     if admin_username not in users or users[admin_username]['access_level'] != 'admin':
+#         abort(404)
+#     if user_username not in users:
+#         abort(404)
+#
+#     # здесь собираем статистику для пользователя user_username
+#     # и передаем ее в шаблон для отображения
+#     user_stats = {
+#         'username': user_username,
+#         'field1': "asfsaafsasaf",  # заполни данные для поля 1
+#         'field2': "asfsaafsasasdadaf",  # заполни данные для поля 2
+#     }
+#     return render_template('user_stats.html', username=admin_username, user_username=user_username, user_stats=user_stats)
 
 
 if __name__ == '__main__':
